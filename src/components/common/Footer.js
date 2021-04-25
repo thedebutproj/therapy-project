6390b2ced191d3b20c5d0640d19d1b3dace78332
Nikhil <agarwalnikhil569@gmail.com>
2021-04-26 03:52:29 +0530

    Added icons in footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,24 +1,47 @@
-import "./Footer.css"
+import "./Footer.css";
 
-const Footer = ()=>{
-    return (
-      <footer>
-        <div className="footer-left">
-          <li> <strong>Links</strong> </li>
-          <li> Work With Us </li>
-          <li> For Mental Health Professionals </li>
-          <li> Helplines </li>
-        </div>
-        <div className="footer-right">
-          <ul>
-            <li> <strong>Contact Us</strong> </li>
-            <li>9876543210</li>
-            <li>ask@thedebutproj.in</li>
-            <li>IN FB LI</li>
-          </ul>
-        </div>
-      </footer>
-    );
-}
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-left">
+        <li>
+          {" "}
+          <strong>Links</strong>{" "}
+        </li>
+        <li> Work With Us </li>
+        <li> For Mental Health Professionals </li>
+        <li> Helplines </li>
+      </div>
+      <div className="footer-right">
+        <ul>
+          <li>
+            {" "}
+            <strong>Contact Us</strong>{" "}
+          </li>
+          <li>9876543210</li>
+          <li>ask@thedebutproj.in</li>
+        </ul>
+
+        <ul className="footer-social-media-icons">
+          <li className="footer-facebook">
+            <ion-icon name="logo-facebook">
+              <a href="#"></a>
+            </ion-icon>
+          </li>
+          <li className="footer-instagram">
+            <ion-icon name="logo-instagram">
+              <a href="#"></a>
+            </ion-icon>
+          </li>
+          <li className="footer-linkedin">
+            <ion-icon name="logo-linkedin">
+              <a href="#"></a>
+            </ion-icon>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -63,13 +63,23 @@ function ContactUs() {
           </form>
         </div>
         <div className="contactus-right">
-            <h2>Please contact us for more information</h2>
-            <ul>
-              <li>Email: <a href="#">thedebut@gmail.com</a> </li>
-              <li>Phone: <a href="#">+91 98 7654 3210</a> </li>
-              <li>Fax: <a href="#"> +91 98 7654 3210</a></li>
-              <li>Address: Block A, 4th Floor, Phoenix Primea (Red Brick Building), Plot No - 40 & 41, Financial District, Beside IRDA, Gachibowli, Hyderabad – 500032, Telangana, India.</li>
-            </ul>
+          <h2>Please contact us for more information</h2>
+          <ul>
+            <li>
+              Email: <a href="#">thedebut@gmail.com</a>{" "}
+            </li>
+            <li>
+              Phone: <a href="#">+91 98 7654 3210</a>{" "}
+            </li>
+            <li>
+              Fax: <a href="#"> +91 98 7654 3210</a>
+            </li>
+            <li>
+              Address: Block A, 4th Floor, Phoenix Primea (Red Brick Building),
+              Plot No - 40 & 41, Financial District, Beside IRDA, Gachibowli,
+              Hyderabad – 500032, Telangana, India.
+            </li>
+          </ul>
         </div>
       </div>
     </div>
