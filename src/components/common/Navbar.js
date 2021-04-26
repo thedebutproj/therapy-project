4d38b99e0b46def557dc789220ebc45693a2431a
mewaraprakhar <pmewara@ee.iitr.ac.in>
2021-04-26 08:56:51 +0530

    navbar styled partially

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -3,14 +3,19 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav id="navbar">
       <div className="navbar-logo">
         <img src={siteLogo}></img>
       </div>
       <div className="navbar-container">
-        <div className="navbar-left">Hiii</div>
-        <div className="navbar-right">Site</div>
+        <li className="nav-items"> <a href=""> Home</a></li>
+        <li className="nav-items"> <a href="">Articles</a> </li>
+        <li className="nav-items"> <a href="">Find a Therapist</a> </li>
+        <li className="nav-items"> <a href="">Blog</a> </li>
+        <li className="nav-items"> <a href="">Donor/Donees</a> </li>
+        <li className="nav-items"> <a href=""> Contact Us</a></li>
       </div>
+    
     </nav>
   );
 };
