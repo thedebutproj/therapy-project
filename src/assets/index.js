062db0ab90278491272c890e8a8bcd7852beb577
Nikhil <agarwalnikhil569@gmail.com>
2021-05-09 22:29:41 +0530

    Made changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import siteLogo from "./Logo.png";
-import homeBlog from "./home-blog.jpg";
-=======
-import siteLogo from "./3f3dd9219f7bb1c9617cf4f154b70383.jpg";
 import homeBlog from "./home-blog.png";
->>>>>>> af786750e750abd32df3803d50983c50157d12e0
 import homeDirectory from "./home-directory.jpg";
 import homeDonor from "./home-donor.jpg";
 
