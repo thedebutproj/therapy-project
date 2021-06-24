2575bec36c7ac0ca3bf03b1a3741465a6cab2947
mewaraprakhar <pmewara@ee.iitr.ac.in>
2021-06-24 11:38:19 +0530

    work

--- src/components/pages/Donor.js
+++ src/components/pages/Donor.js
@@ -17,7 +17,7 @@ const Donor = () => {
                         <p>BENEFITS</p>
                     </div>
                     <div className="donor-benefits-content">
-                        <img src={ donor_BenefitsDetails} alt="" />
+                        <img src={donor_BenefitsDetails} alt="" />
                         <p>For most, the decision to seek professional help for their mental wellbeing is a daunting task. While there are many factors at play, the two major variables in the equation - especially for students and young professionals - are information asymmetry and financial constraints. While we strongly believe that through our curated content and our verified directory, we can address the former, we're also passionate about trying to tackle the latter.</p>
                         <p>Via Benefit, The Debut Project would like to assist first-time therapy seekers gain access to the mental healthcare they deserve, by providing ways to obtain monetary aid for their sessions. If you are a student, young-professional, or someone who does not have the resources to pay fully for professional help, please reach out to us via the form at the bottom of this page.</p>
                         <p>While we are launching this humble philanthropic initiative for students and graduates of IIT Roorkee only, we look forward to expanding the scope this idea soon. Hence, we also welcome our visitors to discuss and consider contributing to this cause, and hence widen the scope of prospective beneficiary.</p>
@@ -51,10 +51,16 @@ const Donor = () => {
                         </div>
                         <div className="donor-register-form-fill">
                             <form action="" method="post">
-                                <input type="text" placeholder="Name"/>
-                                <input type="email" placeholder="Email"/>
+                                <input type="text" placeholder="Name" />
+                                <input type="email" placeholder="Email" />
                                 <input type="text" placeholder="Contact Number" />
-                                <input type="text" placeholder="I would like to..(Pledge, Receive, Donate)" />
+                                {/* <input type="text" placeholder="I would like to..(Pledge, Receive, Donate)" /> */}
+                                <select name="choice" id="choice">
+                                <option value="" disabled selected>I would like to..(Pledge, Receive, Donate)</option>
+                                    <option value="Pledge">Pledge</option>
+                                    <option value="Receive">Receive</option>
+                                    <option value="Donate">Donate</option>
+                                </select>
                                 <textarea name="" id="" cols="30" rows="10" placeholder="Message for our team"></textarea>
                                 <button>Submit</button>
                             </form>
@@ -62,7 +68,7 @@ const Donor = () => {
                         </div>
                     </div>
                     <div className="donor-register-form-right">
-                      <img src={donor_MainIllustration} alt="" />
+                        <img src={donor_MainIllustration} alt="" />
                     </div>
                 </div>
             </div>
